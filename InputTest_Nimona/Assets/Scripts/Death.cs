cdf010c4e69738abacc3961af3c5314185e093fe
Julia Costa <juliacosta232005@gmail.com>
2024-06-07 23:00:49 +0100

    Add test code for GameOver() method in Death script

--- InputTest_Nimona/Assets/Scripts/Death.cs
+++ InputTest_Nimona/Assets/Scripts/Death.cs
@@ -1,12 +1,19 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Death : MonoBehaviour
 {
+    private Scene currentScene;
+    private void Start()
+    {
+        currentScene = SceneManager.GetActiveScene();
+    }
     public void GameOver()
     {
         //GameOver code
         Debug.Log("KILELD PLAYER!");
+        SceneManager.LoadScene(currentScene.name);
     }
 }
