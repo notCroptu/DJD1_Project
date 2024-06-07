c824133a08276b7dc74c01bb5678e4a0c7c682a6
notCroptu <martinsmariana1972@gmail.com>
2024-06-07 19:55:18 +0100

    Fix sword knight prefab bug

--- InputTest_Nimona/Assets/Scripts/CameraFollow.cs
+++ InputTest_Nimona/Assets/Scripts/CameraFollow.cs
@@ -6,9 +6,22 @@ public class cameraFollow : MonoBehaviour
 {
     [SerializeField] private Transform objectToFollow;
     [SerializeField] private float rateToFollow = 0.5f;
-    [SerializeField] private float yOffSet;
+    [SerializeField] private float yOffSetAmount;
+    private float yOffSet;
+    private Movement movement;
+    private bool isGrounded;
     void FixedUpdate()
     {
+        if ( movement == null )
+        {
+            movement = objectToFollow.gameObject.GetComponent<Movement>();
+        }
+        else
+        {
+            isGrounded = movement.IsGrounded;
+        }
+        Debug.Log(isGrounded);
+
         Vector3 targetPosition = objectToFollow.position;
         targetPosition.z = transform.position.z;
 
@@ -16,7 +29,15 @@ public class cameraFollow : MonoBehaviour
 
         transform.position += delta * rateToFollow;
 
-        transform.position = new Vector3(transform.position.x, transform.position.y + yOffSet, transform.position.z);
+        if ( isGrounded )
+        {
+            yOffSet = Mathf.Lerp(yOffSet, yOffSetAmount, 0.01f);
+        }
+        else
+        {
+            yOffSet = Mathf.Lerp(yOffSet, 0, 0.01f);
+        }
 
+        transform.position = new Vector3(transform.position.x, transform.position.y + yOffSet, transform.position.z);
     }
 }
