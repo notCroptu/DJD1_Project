551ee944f0f137b599aafecf66ed7a8ecb44ec3b
notCroptu <martinsmariana1972@gmail.com>
2024-06-07 19:58:46 +0100

    Undo camera follow script logic

--- InputTest_Nimona/Assets/Scripts/CameraFollow.cs
+++ InputTest_Nimona/Assets/Scripts/CameraFollow.cs
@@ -6,21 +6,10 @@ public class cameraFollow : MonoBehaviour
 {
     [SerializeField] private Transform objectToFollow;
     [SerializeField] private float rateToFollow = 0.5f;
-    [SerializeField] private float yOffSetAmount;
-    private float yOffSet;
-    private Movement movement;
+    [SerializeField] private float yOffSet;
     private bool isGrounded;
     void FixedUpdate()
     {
-        if ( movement == null )
-        {
-            movement = objectToFollow.gameObject.GetComponent<Movement>();
-        }
-        else
-        {
-            isGrounded = movement.IsGrounded;
-        }
-        Debug.Log(isGrounded);
 
         Vector3 targetPosition = objectToFollow.position;
         targetPosition.z = transform.position.z;
@@ -28,15 +17,6 @@ public class cameraFollow : MonoBehaviour
         Vector3 delta = targetPosition - transform.position;
 
         transform.position += delta * rateToFollow;
-
-        if ( isGrounded )
-        {
-            yOffSet = Mathf.Lerp(yOffSet, yOffSetAmount, 0.01f);
-        }
-        else
-        {
-            yOffSet = Mathf.Lerp(yOffSet, 0, 0.01f);
-        }
 
         transform.position = new Vector3(transform.position.x, transform.position.y + yOffSet, transform.position.z);
     }
