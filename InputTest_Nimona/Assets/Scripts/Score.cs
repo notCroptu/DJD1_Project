b14fb1f83be12ebec2381d17abc2cbc798008b72
notCroptu <martinsmariana1972@gmail.com>
2024-06-10 00:36:00 +0100

    Change UI fonts

--- InputTest_Nimona/Assets/Scripts/Score.cs
+++ InputTest_Nimona/Assets/Scripts/Score.cs
@@ -7,24 +7,9 @@ using UnityEngine.UI;
 public class Score : MonoBehaviour
 {
     [SerializeField] private TextMeshProUGUI scoreText;
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
     void Update()
     {
-        // if (Input.GetKeyDown(KeyCode.Space))
-        // {
-        //     PlayerScore.ChangeScore(50);
-        // }
-        scoreText.text = $"{PlayerScore.Score}";
+        scoreText.text = $" SCORE {PlayerScore.Score}";
     }
-
-    // private IEnumerator UpdateScore()
-    // {
-
-    // }
 }
